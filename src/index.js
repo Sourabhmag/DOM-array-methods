a8ba0c3c819c4d2b25e80e6b79b4d385d9ec0f4d
Sourabhmag <54775346+Sourabhmag@users.noreply.github.com>
2021-04-26 09:10:41 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,62 @@
+const api = `https://randomuser.me/api`;
+const addUserBtn = document.getElementById("addUserBtn");
+const ascSortBtn = document.getElementById("sort-asc");
+const dscSortBtn = document.getElementById("sort-dsc");
+const userList = document.getElementById("user-list");
+const searchInput = document.getElementById("searchInput");
+
+const appState = [];
+
+addUserBtn.addEventListener("click", async () => {
+  const userData = await fetch(api, {
+    method: "GET"
+  });
+  const userJson = await userData.json();
+  const user = userJson.results[0];
+  appState.push(user);
+  console.log(appState);
+  domRenderer(appState);
+});
+
+const domRenderer = (stateArr) => {
+  userList.innerHTML = null;
+  stateArr.forEach((user) => {
+    const userEl = document.createElement("div");
+    userEl.innerHTML = `<div>
+    Name : ${user.name.title} ${user.name.first} ${user.name.last}
+    <ol>
+      <li>${user.gender}</li>
+      <li>${user.email}</li>
+    </ol
+    </div>`;
+    userList.appendChild(userEl);
+  });
+};
+
+searchInput.addEventListener("keyup", (e) => {
+  console.log(searchInput.value);
+  const filteredData = appState.filter(
+    (user) =>
+      user.name.first.toLowerCase().includes(searchInput.value.toLowerCase()) ||
+      user.name.last.toLowerCase().includes(searchInput.value.toLowerCase()) ||
+      user.email.toLowerCase().includes(searchInput.value.toLowerCase()) ||
+      user.gender.toLowerCase().includes(searchInput.value.toLowerCase())
+  );
+  domRenderer(filteredData);
+});
+
+dscSortBtn.addEventListener("click", () => {
+  const appStateCopy = [...appState];
+  appStateCopy.sort((a, b) => a.name.first < b.name.first ? 1:-1);
+  console.log(appStateCopy);
+  
+  domRenderer(appStateCopy);
+});
+
+ascSortBtn.addEventListener("click", () => {
+  const appStateCopy = [...appState];
+  appStateCopy.sort((a, b) => a.name.first < b.name.first ? -1:1);
+  console.log(appStateCopy);
+  
+  domRenderer(appStateCopy);
+});
